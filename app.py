de7254b3e9811783d2e94688fd5c9289fdeaba6a
zheniagnedchik <46835298+zheniagnedchik@users.noreply.github.com>
2023-10-17 15:52:31 +0300

    add

--- app.py
+++ app.py
@@ -139,76 +139,76 @@ def assign_positions(game_info, positions_by_count):
     return game_info
 
 
-# def calculate_raise_frequencies(hands, positions_group, max_bb=40, min_bb=0, min_bet_bb=0, max_bet_bb=5, min_seat=7, max_seat=9):
-#     frequencies = {}
+def calculate_raise_frequencies(hands, positions_group, max_bb=40, min_bb=0, min_bet_bb=0, max_bet_bb=5, min_seat=7, max_seat=9):
+    frequencies = {}
 
-#     for position, desired_positions in positions_group.items():
-#         raise_opportunity_count = 0
-#         raise_count = 0
+    for position, desired_positions in positions_group.items():
+        raise_opportunity_count = 0
+        raise_count = 0
 
-#         for hand in hands:
-#             players = hand.get('players')
-#             if not players or len(players) < min_seat or len(players) > max_seat:
-#                 continue  # Skip hands that don't have player information or don't have 7 to 9 players
+        for hand in hands:
+            players = hand.get('players')
+            if not players or len(players) < min_seat or len(players) > max_seat:
+                continue  # Skip hands that don't have player information or don't have 7 to 9 players
 
-#             actions_preflop = hand.get('actions', {}).get('preflop', [])
-#             big_blind_value = 0
-#             for action in actions_preflop:
-#                 if 'posts big blind' in action:
-#                     big_blind_value = int(action.split(
-#                         '[')[-1].rstrip(']').replace(',', ''))
-#                     break
+            actions_preflop = hand.get('actions', {}).get('preflop', [])
+            big_blind_value = 0
+            for action in actions_preflop:
+                if 'posts big blind' in action:
+                    big_blind_value = int(action.split(
+                        '[')[-1].rstrip(']').replace(',', ''))
+                    break
 
-#             if big_blind_value == 0:
-#                 continue  # Skip hands that don't have big blind information
+            if big_blind_value == 0:
+                continue  # Skip hands that don't have big blind information
 
-#             for desired_position in desired_positions:
-#                 position_player = None
-#                 player_chips = 0
-#                 for player_info in players:
-#                     if player_info.get('position') == desired_position:
-#                         position_player = player_info['name']
-#                         player_chips = player_info.get('chips', 0)
-#                         break
+            for desired_position in desired_positions:
+                position_player = None
+                player_chips = 0
+                for player_info in players:
+                    if player_info.get('position') == desired_position:
+                        position_player = player_info['name']
+                        player_chips = player_info.get('chips', 0)
+                        break
 
-#                 stack_size_in_bb = player_chips / big_blind_value
-#                 if position_player is None or stack_size_in_bb > max_bb or stack_size_in_bb < min_bb:
-#                     continue  # Skip if player at the desired position is not found or stack size is out of bounds
+                stack_size_in_bb = player_chips / big_blind_value
+                if position_player is None or stack_size_in_bb > max_bb or stack_size_in_bb < min_bb:
+                    continue  # Skip if player at the desired position is not found or stack size is out of bounds
 
-#                 player_action_found = False
-#                 player_action = None
-#                 player_all_in = False
-#                 for action in actions_preflop:
-#                     if f'Dealt to {position_player}' in action:
-#                         player_action_found = True
-#                         continue
-#                     if player_action_found:
-#                         if position_player in action:
-#                             player_action = action
-#                             if 'all-in' in action:
-#                                 player_all_in = True
-#                             break
+                player_action_found = False
+                player_action = None
+                player_all_in = False
+                for action in actions_preflop:
+                    if f'Dealt to {position_player}' in action:
+                        player_action_found = True
+                        continue
+                    if player_action_found:
+                        if position_player in action:
+                            player_action = action
+                            if 'all-in' in action:
+                                player_all_in = True
+                            break
 
-#                 if player_action and not player_all_in:
-#                     actions_before_player = actions_preflop[:actions_preflop.index(
-#                         player_action)]
-#                     if all(not 'calls' in action and not 'raises' in action and not 'all-in' in action for action in actions_before_player):
-#                         raise_opportunity_count += 1
+                if player_action and not player_all_in:
+                    actions_before_player = actions_preflop[:actions_preflop.index(
+                        player_action)]
+                    if all(not 'calls' in action and not 'raises' in action and not 'all-in' in action for action in actions_before_player):
+                        raise_opportunity_count += 1
 
-#                         if 'raises' in player_action:
-#                             bet_size = int(player_action.split(
-#                                 '[')[-1].rstrip(']').replace(',', ''))
-#                             bet_size_in_bb = bet_size / big_blind_value
-#                             if min_bet_bb <= bet_size_in_bb <= max_bet_bb:
-#                                 raise_count += 1
+                        if 'raises' in player_action:
+                            bet_size = int(player_action.split(
+                                '[')[-1].rstrip(']').replace(',', ''))
+                            bet_size_in_bb = bet_size / big_blind_value
+                            if min_bet_bb <= bet_size_in_bb <= max_bet_bb:
+                                raise_count += 1
 
-#         raise_frequency = 0
-#         if raise_opportunity_count > 0:
-#             raise_frequency = (raise_count / raise_opportunity_count) * 100
+        raise_frequency = 0
+        if raise_opportunity_count > 0:
+            raise_frequency = (raise_count / raise_opportunity_count) * 100
 
-#         frequencies[position] = raise_frequency
+        frequencies[position] = raise_frequency
 
-#     return json.dumps(frequencies)
+    return json.dumps(frequencies)
 
 
 # def calculate_raise_frequencies_all_inn(hands, positions_group, max_bb=40, min_bb=0, min_bet_bb=0, max_bet_bb=5, min_seat=7, max_seat=9):
@@ -476,38 +476,42 @@ def upload_file():
         'CO': ('CO',),
         'BTN': ('BTN',),
         'SB': ('SB',),
-        'BB': ('BB')
+        # 'BB': ('BB')
     }
+
     params = request.form.get('params')
     player_name = request.form.get('player_name')
     if not params:
         return jsonify(error='No parameters provided'), 400
     try:
-        params = json.loads(params)
+        params_list = json.loads(params)  # Теперь это список параметров
     except json.JSONDecodeError:
         return jsonify(error='Invalid parameters format'), 400
 
     results = []
-    for param in params:
-        max_bb = param.get('max_bb')
-        min_bb = param.get('min_bb')
-        min_bet_bb = param.get('min_bet_bb')
-        max_bet_bb = param.get('max_bet_bb')
-        min_seat = param.get('min_seat')
-        max_seat = param.get('max_seat')
-        title = param.get('title')
+    for params in params_list:  # Итерация по списку параметров
+        for param in params['value']:
+            max_bb = param.get('max_bb')
+            min_bb = param.get('min_bb')
+            min_bet_bb = param.get('min_bet_bb')
+            max_bet_bb = param.get('max_bet_bb')
+            min_seat = param.get('min_seat')
+            max_seat = param.get('max_seat')
+            title = param.get('title')
 
-        # Проверка, что все параметры предоставлены
-        if any(param is None for param in [max_bb, min_bb, min_bet_bb, max_bet_bb, title, min_seat, max_seat, player_name]):
-            return jsonify(error='Missing one or more parameters'), 400
+            if any(param is None for param in [max_bb, min_bb, min_bet_bb, max_bet_bb, title, min_seat, max_seat, player_name]):
+                return jsonify(error='Missing one or more parameters'), 400
 
-        frequencies_json = calculate_raise_frequencies(
-            parsed_games, positions_group, player_name, max_bb, min_bb, min_bet_bb, max_bet_bb, min_seat, max_seat,
-        )
+            frequencies_json = calculate_raise_frequencies(
+                parsed_games, positions_group, player_name, max_bb, min_bb, min_bet_bb, max_bet_bb, min_seat, max_seat,
+            )
 
-        result = json.loads(frequencies_json)
-        result['title'] = title  # Добавление title в результат
-        results.append(result)
+            result = json.loads(frequencies_json)
+            result['title'] = title
+            result['category'] = params['title']
+            result['title_eader'] = params['titleHeader']
+            result['table_title'] = params['table_title']
+            results.append(result)
 
     return jsonify(data=results)
 
@@ -517,8 +521,70 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/allin_6_9', methods=['POST'])
-def upload_files():
+# @app.route('/allin_6_9', methods=['POST'])
+# def upload_files():
+#     if 'file' not in request.files:
+#         return jsonify(error='No file part'), 400
+#     file = request.files['file']
+#     if file.filename == '':
+#         return jsonify(error='No selected file'), 400
+
+#     filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+#     file.save(filename)
+
+#     with open(filename, 'r', encoding="utf-8") as f:
+#         text = f.read()
+
+#     parsed_games = parse_hands(text, positions_by_count)
+
+#     positions_group = {
+#         'EP': ('UTG+1', 'UTG+2'),
+#         'MP': ('MP+1', 'LJ'),
+#         'HJ': ('HJ',),
+#         'CO': ('CO',),
+#         'BTN': ('BTN',),
+#         'SB': ('SB',),
+#         # 'BB': ('BB')
+#     }
+
+#     params = request.form.get('params')
+#     player_name = request.form.get('player_name')
+#     if not params:
+#         return jsonify(error='No parameters provided'), 400
+#     try:
+#         params_list = json.loads(params)  # Теперь это список параметров
+#     except json.JSONDecodeError:
+#         return jsonify(error='Invalid parameters format'), 400
+
+#     results = []
+#     for params in params_list:  # Итерация по списку параметров
+#         for param in params['value']:
+#             max_bb = param.get('max_bb')
+#             min_bb = param.get('min_bb')
+#             min_bet_bb = param.get('min_bet_bb')
+#             max_bet_bb = param.get('max_bet_bb')
+#             min_seat = param.get('min_seat')
+#             max_seat = param.get('max_seat')
+#             title = param.get('title')
+
+#             if any(param is None for param in [max_bb, min_bb, min_bet_bb, max_bet_bb, title, min_seat, max_seat, player_name]):
+#                 return jsonify(error='Missing one or more parameters'), 400
+
+#             frequencies_json = calculate_raise_frequencies(
+#                 parsed_games, positions_group, player_name, max_bb, min_bb, min_bet_bb, max_bet_bb, min_seat, max_seat,
+#             )
+
+#             result = json.loads(frequencies_json)
+#             result['title'] = title
+#             result['category'] = params['title']
+#             result['title_eader'] = params['titleHeader']
+#             result['table_title'] = params['table_title']
+#             results.append(result)
+
+#     return jsonify(data=results)
+
+
+def process_upload(calc_func):
     if 'file' not in request.files:
         return jsonify(error='No file part'), 400
     file = request.files['file']
@@ -540,40 +606,49 @@ def upload_files():
         'CO': ('CO',),
         'BTN': ('BTN',),
         'SB': ('SB',),
-        'BB': ('BB')
+
     }
+
     params = request.form.get('params')
     player_name = request.form.get('player_name')
     if not params:
         return jsonify(error='No parameters provided'), 400
     try:
-        params = json.loads(params)
+        params_list = json.loads(params)  # Теперь это список параметров
     except json.JSONDecodeError:
         return jsonify(error='Invalid parameters format'), 400
 
     results = []
-    for param in params:
-        max_bb = param.get('max_bb')
-        min_bb = param.get('min_bb')
-        min_bet_bb = param.get('min_bet_bb')
-        max_bet_bb = param.get('max_bet_bb')
-        min_seat = param.get('min_seat')
-        max_seat = param.get('max_seat')
-        title = param.get('title')
+    for params in params_list:  # Итерация по списку параметров
+        for param in params['value']:
+            max_bb = param.get('max_bb')
+            min_bb = param.get('min_bb')
+            min_bet_bb = param.get('min_bet_bb')
+            max_bet_bb = param.get('max_bet_bb')
+            min_seat = param.get('min_seat')
+            max_seat = param.get('max_seat')
+            title = param.get('title')
 
-        # Проверка, что все параметры предоставлены
-        if any(param is None for param in [max_bb, min_bb, min_bet_bb, max_bet_bb, title, min_seat, max_seat, ]):
-            return jsonify(error='Missing one or more parameters'), 400
+            if any(param is None for param in [max_bb, min_bb, min_bet_bb, max_bet_bb, title, min_seat, max_seat, player_name]):
+                return jsonify(error='Missing one or more parameters'), 400
 
-        frequencies_json = calculate_raise_frequencies_for_player(
-            parsed_games, positions_group, player_name,  max_bb, min_bb, min_bet_bb, max_bet_bb, min_seat, max_seat,
-        )
+            frequencies_json = calc_func(
+                parsed_games, positions_group, player_name, max_bb, min_bb, min_bet_bb, max_bet_bb, min_seat, max_seat,
+            )
 
-        result = json.loads(frequencies_json)
-        result['title'] = title  # Добавление title в результат
-        results.append(result)
+            result = json.loads(frequencies_json)
+            result['title'] = title
+            result['category'] = params['title']
+            result['title_eader'] = params['titleHeader']
+            result['table_title'] = params['table_title']
+            results.append(result)
 
     return jsonify(data=results)
+
+
+@app.route('/allin_6_9', methods=['POST'])
+def allInn():
+    return process_upload(calculate_raise_frequencies_for_player)
 
 
 if __name__ == '__main__':
